e08facd7d91ad9b8d1adfd66b2234a211c47cbb9
Ariana Hwang <ajhwang0717@gmail.com>
2019-11-26 21:10:46 -0500

    add student to db works

--- browser/components/NewStudentForm.js
+++ browser/components/NewStudentForm.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
+import Axios from 'axios'
 
 export default class NewStudentForm extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  async handleChange(){
+    this.setState({
+      [event.target.name] : event.target.value
+    })
+  }
+
+  async handleSubmit(event){
+    event.preventDefault()
+    const res = await Axios.post('/student', this.state)
+
+    this.setState({
+      firstName: '',
+      lastName: '',
+      email: ''
+    })
+  }
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
           First Name:
-          <input type="text" name="firstName" />
+          <input type="text" name="firstName" onChange = {this.handleChange} value={this.state.firstName} />
         </label>
 
         <label>
           Last Name:
-          <input type="text" name="lastName" />
+          <input type="text" name="lastName" onChange = {this.handleChange} value={this.state.lastName}/>
         </label>
 
         <label>
           Email:
-          <input type="email" name="email" />
+          <input type="email" name="email" onChange = {this.handleChange} value={this.state.email}/>
         </label>
 
         <button type="submit">Submit New Student</button>
